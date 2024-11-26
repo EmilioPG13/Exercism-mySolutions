e0345b03f07caf09c2582c03e6135937c95691a3
Emilio <emilio.pg95@gmail.com>
2024-11-26 07:08:08 -0600

    Implement visitor creation, ticket revocation, and ticket status functions in amusement-park exercise

--- javascript/amusement-park/amusement-park.js
+++ javascript/amusement-park/amusement-park.js
@@ -10,7 +10,12 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Please implement the createVisitor function.');
+  let newVisitor = {
+    name: name, 
+    age: age, 
+    ticketId: ticketId
+  }
+  return newVisitor;
 }
 
 /**
@@ -20,7 +25,8 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Please implement the revokeTicket function.');
+  visitor.ticketId = null;
+  return visitor;
 }
 
 /**
@@ -31,7 +37,13 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Please implement the ticketStatus function.');
+  if (!(ticketId in tickets)) {
+    return 'unknown ticket id';
+  } else if (tickets[ticketId] === null) {
+    return 'not sold';
+  } else {
+    return `sold to ${tickets[ticketId]}`;
+  }
 }
 
 /**
@@ -43,7 +55,11 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Please implement the simpleTicketStatus function.');
+  if (tickets[ticketId] !== null && tickets[ticketId] !== undefined) {
+    return tickets[ticketId];
+  } else {
+    return 'invalid ticket !!!';
+  }
 }
 
 /**
@@ -53,5 +69,8 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Please implement the gtcVersion function.');
+  if (visitor.gtc && visitor.gtc.version) {
+    return visitor.gtc.version;
+  }
+  return undefined;
 }
