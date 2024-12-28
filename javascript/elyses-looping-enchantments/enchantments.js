72219ccba25c807e6c71e8796823393b583342e7
Emilio <emilio.pg95@gmail.com>
2024-12-28 14:07:51 -0600

    Implement card type and odd/even card counting functions

--- javascript/elyses-looping-enchantments/enchantments.js
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -9,7 +9,15 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  throw new Error('Implement the cardTypeCheck function');
+  let count = 0
+
+  stack.forEach((currentCard) => {
+    if (currentCard === card) {
+      count++;
+    }
+  });
+
+  return count
 }
 
 /**
@@ -20,5 +28,14 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  throw new Error('Implement the determineOddEvenCards function');
+  let count = 0;
+  
+  stack.forEach((num) => {
+    if (type && num % 2 === 0) {
+      count++;
+    } else if (!type && num % 2 !== 0) {
+      count++;
+    }
+  });
+  return count;
 }
