c35cf4c72d36b90d4c7633948308554f8fb82e04
Emilio <emilio.pg95@gmail.com>
2025-01-08 22:21:02 -0600

    Implement card manipulation functions: seeingDouble, threeOfEachThree, middleTwo, sandwichTrick, twoIsSpecial, perfectlyOrdered, and reorder

--- javascript/elyses-transformative-enchantments/enchantments.js
+++ javascript/elyses-transformative-enchantments/enchantments.js
@@ -8,7 +8,8 @@
  * @returns {number[]} deck with every card doubled
  */
 export function seeingDouble(deck) {
-  throw new Error('Implement the seeingDouble function');
+  const doubleNum = deck.map((value) => value * 2);
+  return doubleNum;
 }
 
 /**
@@ -19,7 +20,14 @@ export function seeingDouble(deck) {
  * @returns {number[]} deck with triplicate 3s
  */
 export function threeOfEachThree(deck) {
-  throw new Error('Implement the threeOfEachThree function');
+  return deck.reduce((acc, value) => {
+    if (value === 3) {
+      acc.push(3, 3, 3);
+    } else {
+      acc.push(value);
+    }
+    return acc;
+  }, []);
 }
 
 /**
@@ -31,7 +39,7 @@ export function threeOfEachThree(deck) {
  * @returns {number[]} deck with only two middle cards
  */
 export function middleTwo(deck) {
-  throw new Error('Implement the middleTwo function');
+  return deck.slice(4, 6);
 }
 
 /**
@@ -43,7 +51,13 @@ export function middleTwo(deck) {
  */
 
 export function sandwichTrick(deck) {
-  throw new Error('Implement the sandwichTrick function');
+  const first = deck.shift();
+  const last = deck.pop();
+
+  const middleIndex = Math.floor(deck.length / 2);
+  deck.splice(middleIndex, 0, last, first);
+
+  return deck;
 }
 
 /**
@@ -54,7 +68,7 @@ export function sandwichTrick(deck) {
  * @returns {number[]} deck with only 2s
  */
 export function twoIsSpecial(deck) {
-  throw new Error('Implement the twoIsSpecial function');
+  return deck.filter((two) => two === 2 );
 }
 
 /**
@@ -65,7 +79,7 @@ export function twoIsSpecial(deck) {
  * @returns {number[]} ordered deck
  */
 export function perfectlyOrdered(deck) {
-  throw new Error('Implement the perfectlyOrdered function');
+  return deck.sort((a, b) => a - b);
 }
 
 /**
@@ -76,5 +90,5 @@ export function perfectlyOrdered(deck) {
  * @returns {number[]} reordered deck
  */
 export function reorder(deck) {
-  throw new Error('Implement the reorder function');
+  return deck.reverse();
 }
